35e143c1c1b368d1af661d298e5633723bc6d898
Claudio Caporaso <claudiocaporaso1998@gmail.com>
2023-10-02 12:47:16 +0200

    non riesco a capire per quale motivo il codice non mi stampa e non mi crea le view su heidi

---
+++ Student.java
@@ -0,0 +1,22 @@
+import java.sql.*;
+import java.util.ArrayList;
+
+public class Student {
+    String name;
+    String surname;
+
+    public Student(String name, String surname) {
+        this.name = name;
+        this.surname = surname;
+    }
+
+    public String getName(String name) {
+        return name;
+    }
+
+    public String getSurname(String surname) {
+        return surname;
+    }
+}
+
+
